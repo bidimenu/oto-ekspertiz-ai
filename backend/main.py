d70cafb2f2a95b718d1db338be8a2c4959b04a7d
Yeni Kullanıcı Adın <saashocam@gmail.com>
2026-02-28 17:19:22 +0300

    test

--- backend/main.py
+++ backend/main.py
@@ -133,8 +133,8 @@ async def arac_analiz_et(
 
         import google.generativeai as genai # Gerektiğinde import et
         genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-        model = genai.GenerativeModel('gemini-1.5-flash')
-
+        #model = genai.GenerativeModel('gemini-1.5-flash')
+        model = genai.GenerativeModel("models/gemini-1.5-flash")
 
         try:
             print("🌟 GEMINI ANALİZİ BAŞLADI...")
